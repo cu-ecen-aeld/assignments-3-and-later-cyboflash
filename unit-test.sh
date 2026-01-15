b5a7f4ad0e3077422306cc7ac1731d67c7b9b42e
cyboflash <2373916+cyboflash@users.noreply.github.com>
2026-01-15 00:12:38 -0800

    Add debugging to unit-test.sh

--- unit-test.sh
+++ unit-test.sh
@@ -4,10 +4,18 @@
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
+set -e
+set -x
 mkdir -p build
 cd build
 cmake ..
 make clean
 make
 cd ..
+pwd
+ls -la build/assignment-autotest
 ./build/assignment-autotest/assignment-autotest
+rc=$?
+echo "Exit code of assignment-autotest is $rc"
+exit $rc
+
